9db02837c09e7cb78564c6021cfefc2a0e826bf4
Mauricio Lopez <mauroagustin.lopez.456@gmail.com>
2026-05-11 23:48:23 -0300

    feat: integrate @nestjs/swagger for API documentation and error handling; update DTOs to use new imports

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,31 @@ import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AppExceptionFilter } from './common/filters/app-exception.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ErrorResponseDto } from './common/dto/error-response.dto';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('MartinaML store api')
+    .setDescription('The MartinaML store API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .addGlobalResponse({
+      status: 500,
+      description: 'Internal server error',
+      type: ErrorResponseDto,
+    })
+    .addGlobalResponse({
+      status: '4XX',
+      description: 'Default error response',
+      type: ErrorResponseDto,
+    })
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   app.useGlobalFilters(new AppExceptionFilter());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
